07fc109133f01f8b23e00f614d99489dc27365fe
hepeng <965197905@qq.com>
2022-01-17 21:58:04 +0800

    fix:store

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import http from './http/http'
 import router from './router/index'
 import 'amfe-flexible'
 import 'virtual:svg-icons-register'
+import store from './store'
 const app = createApp(App)
 
-app.use(router)
+app.use(router).use(store)
 app.config.globalProperties.$http = http
 app.mount('#app')
