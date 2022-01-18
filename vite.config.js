afd0aea1929925c5ac334210f585a415a4645230
hepeng <965197905@qq.com>
2022-01-18 21:03:12 +0800

    fix:打包

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,7 @@ export default defineConfig({
             strict: false,
         },
     },
+    base: './',
     resolve: {
         // https://vitejs.dev/config/#resolve-alias
         alias: {
